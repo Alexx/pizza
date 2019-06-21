8968fd4373854a1dea7af7f9a9d6925856239f19
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-21 09:15:55 -0700

    build out Pizza price and addTopping prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
-}
+};
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+};
 
 Pizza.prototype.price = function() {
-
-}
+  let totalPrice = 0;
+  totalPrice += this.size[1];
+  this.toppings.forEach(function(topping) {
+    totalPrice += topping[1];
+  });
+  return totalPrice;
+};
 
 $(document).ready(function(){
-  var myPizza = new Pizza("Medium");
+  let myPizza = new Pizza(["Small", 8]);
   console.log(myPizza);
+  myPizza.addTopping(["Mayonnaise", 1]);
+  console.log(myPizza);
+  myPizza.addTopping(["Bacon", 2]);
+  console.log(myPizza);
+  console.log(myPizza.price());
 });
