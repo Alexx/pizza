e63f5871a1367b146294e2259ba903e13b539fd1
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-21 09:46:54 -0700

    add part of form to order pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+const pizzaSizes = [["Small", 8],
+                    ["Medium", 10],
+                    ["Large", 12],
+                    ["X-Large", 14]];
+const pizzaToppings = [["Pepperoni", 1],
+                       ["Sausage", 1],
+                       ["Chicken", 1.5],
+                       ["Bacon", 2],
+                       ["Mushrooms", 1],
+                       ["Black Olives", 0.5],
+                       ["Green Peppers", 1]];
+
+//Business logic
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
@@ -8,16 +21,16 @@ Pizza.prototype.addTopping = function(topping) {
 };
 
 Pizza.prototype.price = function() {
-  let totalPrice = 0;
-  totalPrice += this.size[1];
+  let totalPrice = this.size[1];
   this.toppings.forEach(function(topping) {
     totalPrice += topping[1];
   });
   return totalPrice;
 };
 
+//User-interface logic
 $(document).ready(function(){
-  let myPizza = new Pizza(["Small", 8]);
+  let myPizza = new Pizza(["Large", 12]);
   console.log(myPizza);
   myPizza.addTopping(["Mayonnaise", 1]);
   console.log(myPizza);
